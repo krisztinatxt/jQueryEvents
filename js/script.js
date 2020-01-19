51d9e8c86cd1d4956d04f0dda99c19c7131fe0e3
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-01-19 12:33:07 +0000

    method challenges

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+$("#button1").mouseenter(function(){
+$('#button1').removeClass("makeRed").addClass("makeBorder");
+});
+
+$("#button1").mouseleave(function(){
+$("#button1").removeClass("makeBorder").addClass("makeRed");
+});
+
+}); 
+
+
+
+}); 
